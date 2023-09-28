ba76e327d730166d86d9c309e5511bf779dce525
JHyun0302 <wogustkfkd11@gmail.com>
2023-09-28 09:17:57 +0900

    [Init] yoloV8 사용 전 set

---
+++ yoloV8Test.py
@@ -0,0 +1,16 @@
+import torch
+
+print("torch version : ", torch.__version__)
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device('cuda:0' if USE_CUDA else 'cpu')
+
+print('CUDA 사용 가능 여부 :', USE_CUDA)
+print('현재 사용 device :', device)
+print('CUDA Index :', torch.cuda.current_device())
+print('GPU 이름 :', torch.cuda.get_device_name())
+print('GPU 개수 :', torch.cuda.device_count())
+
+import ultralytics
+
+ultralytics.checks()
